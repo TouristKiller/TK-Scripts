b361c5fb585ecce2bccd30ab5ee2fbb948d185ed
TouristKiller <kmljanssen@gmail.com>
2025-01-27 13:15:51 +0100

    Update setup script for TK_time selection loop color; change ext state key for consistency

--- Tools/TK_time selection loop color_setup.lua
+++ Tools/TK_time selection loop color_setup.lua
@@ -2,5 +2,5 @@ local script_name = "TK_time selection loop color.lua"
 local resource_path = reaper.GetResourcePath()
 local script_path = resource_path .. "/Scripts/TK-Scripts/Tools/" .. script_name
 
-reaper.SetExtState("TK_Timeselect_Loop_Color", "CALL_SETUP", "1", false)
+reaper.SetExtState("TK_time selection loop color_setup", "CALL_SETUP", "1", false)
 reaper.RefreshToolbar2(0, -1)
